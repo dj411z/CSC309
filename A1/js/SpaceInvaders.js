fac2ddd4d3345b79c0b958e419265225a0ac525e
Fady Anees <aneesfad@gmail.com>
2014-01-31 19:22:04 -0500

    Added Pause state and html

--- A1/js/SpaceInvaders.js
+++ A1/js/SpaceInvaders.js
@@ -1,24 +1,105 @@
-window.onload = function() {
-  canvas = document.getElementById("myCanvas");
-  context = canvas.getContext("2d");
+// --------------------------------------------------
+// --------------------------------------------------
+//  Classes 
+// --------------------------------------------------
+// --------------------------------------------------
 
-  var img = new Image();
-	  
-  img.onload = function () {
-	  context.drawImage(img, -50, -10);
-  }
-  
-  img.src = "start.jpg"; // get the image from this URL	  
-};
-function alien(canvas, x, y){
-
+// This function stores the details for a single alien
+function Alien(canvas, x, y){
   this.context = canvas.getContext("2d");
   this.x = x;
   this.y = y;
 
-}
-aliens = {};
+  this.draw = function () {
 
+    var image = new Image();
+
+    image.onload = function() {
+      context.drawImage(image, this.x, this.y);
+    }
+    image.src = "images/alien.png";
+
+  }
+
+}
+
+// --------------------------------------------------
+// --------------------------------------------------
+//  onLoad calls
+// --------------------------------------------------
+// --------------------------------------------------
+
+var aliens = []; //aliens array holds alien objects
+
+var canvas;
+var context;
+
+window.onload = function() {
+  canvas = document.getElementById("myCanvas");
+  document.onkeydown = checkKey;
+  context = canvas.getContext("2d");
+  
+  var img = new Image();
+    
+  img.onload = function () {
+    context.drawImage(img, -50, -10);
+  }
+  
+  img.src = "images/start.jpg"; // get the image from this URL    
+}
+
+// --------------------------------------------------
+// --------------------------------------------------
+//  Different States Functions 
+// --------------------------------------------------
+// --------------------------------------------------
+
+//Clears canvas and aliens array and starts a new game
 function newGame(){
-	context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  aliens = [];
+  x = 100;
+  y = 100;
+  for(var i=0; i<6; i++){
+    var alien = new Alien(canvas, x, y);
+    aliens.push(alien);
+    x += 40;
+  }
+
+  drawAliens();
+
+}
+
+function pauseGame(){
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "blue";
+  context.font = "bold 40px Arial";
+  context.fillText("Pause", 200, 250);
+}
+
+// --------------------------------------------------
+// --------------------------------------------------
+// Helper Functions
+// --------------------------------------------------
+// --------------------------------------------------
+
+//Draws alien objects inside aliens array
+function drawAliens(){
+  for(var i=0; i<aliens.length; i++){
+    aliens[i].draw();
+  }
+}
+
+function checkKey(e) {
+    var event = window.event ? window.event : e;
+    console.log(event.keyCode);
+    if (event.keyCode == 13) {
+        newGame();
+    }
+    else if (event.keyCode == 80){
+        pauseGame();
+    }
+    else if (event.keyCode == 81){
+        quitGame();
+    }
 }
