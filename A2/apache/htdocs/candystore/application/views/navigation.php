6da1adade960ef814bf494218064b18421272300
dj411z <dj411z@yahoo.com>
2014-03-15 14:12:17 -0400

    Create navigation.php

---
+++ A2/apache/htdocs/candystore/application/views/navigation.php
@@ -0,0 +1,6 @@
+<?php
+ echo anchor('shopping_cart','My Cart') . "<br />";
+ echo anchor('my_account','My Account') . "<br />";
+ echo anchor('candystore','Candystore') . "<br />";
+ echo anchor('logout','Logout') . "<br />";
+ ?>
