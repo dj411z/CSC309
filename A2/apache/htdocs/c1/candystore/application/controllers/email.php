2d360d5139609e1d7a094d260ab4f189b24e2bcc
Jiang Dennis <g3jiangd@cdf.toronto.edu>
2014-03-18 20:34:01 -0400

    Fixed email

--- A2/apache/htdocs/c1/candystore/application/controllers/email.php
+++ A2/apache/htdocs/c1/candystore/application/controllers/email.php
@@ -23,10 +23,19 @@ class Email extends CI_Controller {
     }
     
 	function sendEmail($email) {
-		$this->load->model('email_model');
+		$this->load->library('email');
 
-		$this->email_model->sendEmail($email);
+		$this->email->from('dennis.jiang411z@gmail.com', 'Dennis Jiang');
+		$this->email->to("$email");
 
+		$this->email->subject('Candystore Receipt');
+		$this->email->message('Here is your receipt.';
+
+		//cannot attach receipt file because not a file / hard to do
+
+		$this->email->send();
+
+		echo $this->email->print_debugger();
 		redirect('email/showAll', 'refresh');
 	}
 
--- A2/apache/htdocs/c1/candystore/application/views/email/email.php
+++ A2/apache/htdocs/c1/candystore/application/views/email/email.php
@@ -1,4 +1,4 @@
 <h1>Email</h1>
-<?php 
+<?php
 	echo anchor("email/sendEmail/dj411z@yahoo.com", 'Send email');
 ?>	
